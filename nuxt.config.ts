bc97813befcd64325890be536e8a0bd7fdeaf664
LinHaoBin <1311902424@qq.com>
2023-04-25 16:01:00 +0800

    feat: 测试报错

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 module.exports = {
-  ssr: false,
   nitro: {
     preset: 'vercel-edge',
+  },
+  routeRules: {// Use client-side rendering for all routes
+    '/**': { ssr: false }
   },
   dev: process.env.NODE_ENV !== 'production'
 };
